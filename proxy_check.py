87ff8a2db2ec495e0c580ec276812fdf250c85cb
aishwaryapradhan <aishwaryapradhan82@gmail.com>
2017-03-29 19:09:53 +0530

    made it compatible for python3 by minor library fixes

--- proxy_check.py
+++ proxy_check.py
@@ -1,29 +1,30 @@
-import urllib2, socket
+import urllib.request
+import socket
 
 socket.setdefaulttimeout(180)
 
 # read the list of proxy IPs in proxyList
-proxyList = ['172.30.1.1:8080', '172.30.3.3:8080'] # there are two sample proxy ip
+proxyList = ['103.255.30.249:80', '202.5.17.76:8080',"77.241.235.52:80"] # there are two sample proxy ip
 
 def is_bad_proxy(pip):    
     try:        
-        proxy_handler = urllib2.ProxyHandler({'http': pip})        
-        opener = urllib2.build_opener(proxy_handler)
+        proxy_handler = urllib.request.ProxyHandler({'http': pip})
+        opener = urllib.request.build_opener(proxy_handler)
         opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-        urllib2.install_opener(opener)        
-        req=urllib2.Request('http://www.google.com')  # change the url address here
-        sock=urllib2.urlopen(req)
-    except urllib2.HTTPError, e:        
-        print 'Error code: ', e.code
-        return e.code
-    except Exception, detail:
+        urllib.request.install_opener(opener)
+        req=urllib.request.Request('https://www.google.com')  # change the url address here
+        sock=urllib.request.urlopen(req)
 
-        print "ERROR:", detail
+    except urllib.request.HTTPError as e:
+        print('Error code: ',e)
+        return 1
+    except Exception as detail:
+        print("ERROR:", detail)
         return 1
     return 0
 
 for item in proxyList:
     if is_bad_proxy(item):
-        print "Bad Proxy", item
+        print("Bad Proxy", item)
     else:
-        print item, "is working"
+        print(item, "is working")
